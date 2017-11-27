71bdcbc23d4d97a572292361577d32c096511f82
LE Pearson <chocoluffer@gmail.com>
2017-11-26 17:12:59 -0800

    setup

---
+++ constants.js
@@ -0,0 +1,16 @@
+export const key = 'b5vbyppcrmsqdt8zahsvt9g7epepe4hh';
+export const secret = 'PjYMJ9rkr46cBYqaqxrsyGh7wvrwGFcU';
+
+export const myCode = 'qg9bax349zb32fqaf9tztyz5';
+export const myToken = 'jcem8z9arg3fnz3zm6rb3sb3';
+
+export const baseApi = 'https://us.api.battle.net/wow/';
+
+
+export default {
+	key,
+	secret,
+	myCode,
+	myToken,
+	baseApi,
+};
